bf87e286ea34a50f8875eb7292540dc4fe9c6d6d
Daegybyte <daegybyte@protonmail.ch>
2021-04-26 11:39:00 -0600

    added some comments to Main file to clarify what is happening

--- finalExam/exam3/weirdMain/src/Main.java
+++ finalExam/exam3/weirdMain/src/Main.java
@@ -3,7 +3,8 @@ public class Main {
     public static void main(String[] args) {
         int i = 0;
         increment(i);
-        System.out.println(i);
+        System.out.println(i); //This outputs 0 because i in main and i in increment are in different scopes
+        //System.out.println(increment(i));
     }
 
     public static int increment(int i) {
