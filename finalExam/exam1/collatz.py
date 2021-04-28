ce34e6e867a39811fa162c57c86aca7fc015870b
Daegybyte <daegybyte@protonmail.ch>
2021-04-28 08:51:13 -0600

    minor tweaks

--- finalExam/exam1/collatz.py
+++ finalExam/exam1/collatz.py
@@ -1,15 +1,22 @@
-n = 1
+n = int(input("start n: "))
 numEvens = 0
 numOdds  = 0
+numLoops = 0
 
 
 while(n !=1):
     if(n%2 == 0):
         numEvens+=1
         n = n/2
+        numLoops+=1
+        
     else:
         numOdds+=1
         n = (3*n)+1
+        numLoops+=1
 
 
-print(numEvens,numOdds)
+print(str("\nn: ")+str(n))
+print(str("times even: ")+str(numEvens))
+print(str("times odd: ")+str(numOdds))
+print(str("times looped: ")+str(numLoops))
---
+++ finalExam/exam1/whileLoopCount.py
@@ -0,0 +1,16 @@
+# Q2 exam 1 practice
+# For the following pseudocode, track the value of variables at the end of each iteration (immediately after executing line 05):
+
+# 01  Set result to 1
+# 02  Set counter to 1
+# 03  While (counter < 5)
+# 04      Set result to result * 2
+# 05      Set counter to counter + 1
+
+result = 1
+counter = 1
+
+while(counter < 5):
+    result = result*2
+    counter += 1
+    print(counter, result)
